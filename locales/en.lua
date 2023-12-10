5bd49a6cb164d889f3d8f8046c1b2c5756dca645
duckodas <duckodas@gmail.com>
2023-12-10 16:44:24 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,25 @@
+Locales['en'] = {
+    ["drugmenu_title_context"] = 'Drug Sales',
+    ["drugmenu_title"] = 'Choose Your Drug Type',
+    ["drugmenu_title_desc"] = 'Select the type of drug you want to sell',
+    ["drugmenu_sale_title"] = 'Start Selling',
+    ["drugmenu_sale_title_desc"] = 'Begin selling your chosen drug here',
+    ["drugmenu_sale_stop_title"] = 'Stop Drug Sales',
+    ["drugmenu_sale_stop"] = 'End Sales',
+    ["drugmenu_sale_stop_desc"] = 'Stop selling drugs',
+
+    -- Ecstasy
+    ["ecstasy_context_title"] = 'Ecstasy',
+    ["ecstasy_context_desc"] = 'Press to select ecstasy!',
+    ["ecstasy_context_currentDrug"] = 'Ecstasy',
+
+    -- Joints
+    ["joints_context_title"] = 'Joints',
+    ["joints_context_desc"] = 'Press to select joints!',
+    ["joints_context_currentDrug"] = 'Joints',
+
+    -- Heroin
+    ["heroin_context_title"] = 'Heroin',
+    ["heroin_context_desc"] = 'Press to select heroin!',
+    ["heroin_context_currentDrug"] = 'Heroin',
+}
